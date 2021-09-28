57b9b6f0ed25a1d6cbeddb2a340625ae5faf3548
George Bolt <g.bolt@lancaster.ac.uk>
2021-09-28 17:59:47 +0100

    Doc Updates

--- docs/make.jl
+++ docs/make.jl
@@ -3,4 +3,7 @@ Pkg.activate("C:/users/boltg/.julia/environments/doc_writing")
 
 using Documenter, InteractionNetworkModels
 
-makedocs(sitename="My Documentation")
+makedocs(
+    sitename="My Documentation",
+    format = Documenter.HTML(prettyurls = false),
+    pages=["index.md", "model_sampling.md"] )
