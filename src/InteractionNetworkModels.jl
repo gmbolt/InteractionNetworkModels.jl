96ebc3d48ad4bfaa09bf41d6126238101653f9f2
gmbolt <56736020+gmbolt@users.noreply.github.com>
2021-11-25 10:14:20 +0000

    Updates.

--- src/InteractionNetworkModels.jl
+++ src/InteractionNetworkModels.jl
@@ -2,9 +2,7 @@ module InteractionNetworkModels
 
 using Distributed
 
-# Write your package code here.
-# Alises for referring to Paths/Interaction Sequences/Samples of Interaction Sequences
-# (purely for code readability)
+# Alises for referring to Paths/Interaction Sequences/Samples of Interaction Sequences (purely for readability)
 include("aliases.jl")
 
 # Distances
@@ -37,7 +35,6 @@ include("MCMC/Model Sampling/imcmc_SIS.jl")
 include("MCMC/Model Sampling/imcmc_SIS_gibbs.jl")
 include("MCMC/Model Sampling/imcmc_SIM.jl")
 include("MCMC/Model Sampling/imcmc_SIM_gibbs.jl")
-# include("MCMC/Model Sampling/imcmc_SIS_insert_delete_fast.jl")
 include("MCMC/Model Sampling/imcmc_SPF.jl")
 include("MCMC/Model Sampling/summarys.jl")
 include("MCMC/Posterior Sampling/types_posteriors.jl")
@@ -49,8 +46,5 @@ include("MCMC/Posterior Sampling/iex_SIS.jl")
 include("MCMC/Posterior Sampling/iex_SIM.jl")
 include("MCMC/Posterior Sampling/summarys.jl")
 include("MCMC/Posterior Sampling/predictives.jl")
-# include("MCMC/Posterior Sampling/parallel_iex_interaction_seqs.jl")
-include("MCMC/Posterior Sampling/parallel_helpers.jl")
-
 
 end
