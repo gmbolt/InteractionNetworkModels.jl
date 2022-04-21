0d8aeedbbebb3e4b0b420a6b76c0482b1da203b0
George Bolt <g.bolt@lancaster.ac.uk>
2022-04-21 14:41:46 +0100

    Update aggrgegate_graph.jl

--- src/Data Processing/aggregate_graph.jl
+++ src/Data Processing/aggregate_graph.jl
@@ -32,6 +32,18 @@ function get_aggregate_graph(S::InteractionSequence{Int})
     return A
 end 
 
+function get_aggregate_graph(S::InteractionSequence{Int}, V::Int)
+    A = zeros(Int, V, V)
+    for path in S
+        for i in 1:(length(path)-1)
+            src, dest = (path[i],path[i+1])
+            A[src,dest] += 1
+        end 
+    end 
+    return A
+end 
+
+
 function get_aggregate_graph(S::InteractionSequence{String})
     S_int, rev_map = string_to_int(S)
     return get_aggregate_graph(S_int), rev_map
